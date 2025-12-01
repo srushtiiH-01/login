aff31f9c2fa417eccdf30ad26413958727cbeb3b
SrushtiH <honnangisrushti@gmail.com>
2025-12-01 18:20:40 +0530

    Fix User model import path - lowercase user

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config();
 
-const User = require('./models/User');
+const User = require('./models/user');
 
 const app = express();
 
